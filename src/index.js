e63c7dcfcde785f30126d1f5d478bfd2908b3e5c
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-07-22 23:37:20 +0530

    made it functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hello mf')
+const alltasksdiv = document.querySelector('.all_tasks');
+
+function deletegrid(container)
+{
+    var cells = container.lastElementChild;
+    while(cells)
+    {
+        container.removeChild(cells);
+        cells = container.lastElementChild;
+    }
+}
+
+const button  = document.querySelector('.submit');
+button.addEventListener('click', ()=> {
+    const task = document.querySelector('#floatingInput').value;
+    var new_task = document.createElement('h2');
+    new_task.textContent = task;
+    console.log(new_task)
+    alltasksdiv.append(new_task);
+    console.log(task);
+})
+
+
+console.log(button);
